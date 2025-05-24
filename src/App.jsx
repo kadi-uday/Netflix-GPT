9b5815245f8263d3b250992d4936204240c6d676
Kadi Uday <udaykadi45@gmail.com>
2025-05-24 19:26:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+ 
+  return (
+    <div className="text-3xl font-bold text-blue-800">
+     Hello world!
+        <h1 className="p-2 m-2 text-blue-900 font-bold text-3xl">uday</h1>
+    </div>
+  )
+}
+
+export default App;
